19196fa322b2954e0d400db4e8817f88b97fdac9
exoron <twexoron@gmail.com>
2021-02-15 21:26:06 +0300

    Seminar 2

---
+++ Assets/Scripts/Field/GridHolder.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+
+namespace Field
+{
+    public class GridHolder : MonoBehaviour
+    {
+        [SerializeField]
+        private int m_GridWidth;
+        [SerializeField]
+        private int m_GridHeight;
+
+        private Grid m_Grid;
+
+        private Camera m_Camera;
+
+        private Vector3 m_Offset;
+        
+        private void Awake()
+        {
+            m_Grid = new Grid(m_GridWidth, m_GridHeight);
+            m_Camera = Camera.main;
+
+            float width = m_GridWidth * m_Nodesize;
+            float height = m_GridHeight * m_Nodesize;
+            transform.localScale = new Vector3(
+                width * 0.1f, 
+                1f, 
+                height * 0.1f);
+
+            m_Offset = transform.position - 
+                       new Vector3(width, 0f, height) * 0.5f;
+        }
+
+        [SerializeField]
+        private float m_Nodesize;
+
+        private void Update()
+        {
+            if (m_Grid == null || m_Camera == null)
+            {
+                return;
+            }
+
+            Vector3 mousePosition = Input.mousePosition;
+
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.transform != transform)
+                {
+                    return;
+                }
+
+                Vector3 hitPosition = hit.point;
+                Vector3 difference = hitPosition - m_Offset;
+
+                int x = (int) (difference.x / m_Nodesize);
+                int z = (int) (difference.z / m_Nodesize);
+                
+                Debug.Log(x + " " + z);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(m_Offset, 0.5f);
+        }
+    }
+}
